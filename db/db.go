14f538f8798f844a0b7862943491f3a1bf8cfa86
Bruno Aguirre <yo@brunoaguirre.com>
2013-07-10 23:00:52 -0300

    Handle OPENREDIS url... not really nice

--- db/db.go
+++ db/db.go
@@ -2,21 +2,25 @@ package db
 
 import (
 	"os"
+	"strings"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
 )
 
 var (
-	server   = os.Getenv("OPENREDIS_URL")
-	password = ""
+	server = os.Getenv("OPENREDIS_URL")[8:]
+	parts  = strings.Split(server, "@")
+
+	password  = parts[0][1:]
+	redis_url = parts[1]
 )
 
 var Pool = &redis.Pool{
 	MaxIdle:     10,
 	IdleTimeout: 240 * time.Second,
 	Dial: func() (redis.Conn, error) {
-		c, err := redis.Dial("tcp", server)
+		c, err := redis.Dial("tcp", redis_url)
 		if err != nil {
 			return nil, err
 		}
