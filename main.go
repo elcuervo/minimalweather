0268daf1b4cbdbd35b732c5cd83f454bcd457605
elCuervo <yo@brunoaguirre.com>
2013-12-12 01:00:10 -0200

    Adds new relic

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"github.com/elcuervo/minimalweather/minimalweather"
+	"github.com/yvasiyarov/gorelic"
+
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	agent := gorelic.NewAgent()
+	agent.Verbose = true
+	agent.NewrelicLicense = os.Getenv("NEW_RELIC_LICENSE_KEY")
+	agent.Run()
+
 	handler := minimalweather.Handler()
 	port := ":" + os.Getenv("PORT")
 	http.Handle("/", handler)
