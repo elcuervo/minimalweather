fefdec899a912f20117611baed145a13e53e4f3b
elCuervo <yo@brunoaguirre.com>
2013-12-13 19:48:40 -0200

    Adds a reminder to add it to the home

--- website/Gruntfile.js
+++ website/Gruntfile.js
@@ -31,6 +31,7 @@ module.exports = function(grunt) {
             "assets/css/pure.css",
             "assets/css/icons.css",
             "assets/css/styles.css",
+            "assets/css/add2home.css",
 
             "assets/js/libs/snapjs/snap.css"
           ]
@@ -49,6 +50,7 @@ module.exports = function(grunt) {
           "assets/js/browser_detect.js",
           "assets/js/app.js",
           "assets/js/zepto.min.js",
+          "assets/js/add2home.js",
           "assets/js/libs/snapjs/snap.js"
         ],
         dest: "assets/js/<%= pkg.name %>.min.js"
